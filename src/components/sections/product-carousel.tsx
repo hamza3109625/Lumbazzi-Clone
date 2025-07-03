0813039f3e4e1a09f6d1d9bef53797a6b746784b
Hamza Nizardeen <hamzanizardeen@gmail.com>
2025-07-03 16:39:39 +0530

    Carousel Done

--- src/components/sections/product-carousel.tsx
+++ src/components/sections/product-carousel.tsx
@@ -11,8 +11,8 @@ import Button from "@/components/custom/ui/button";
 
 export function ProductCard() {
   return (
-    <Card className="group relative rounded-[40px] overflow-hidden text-white">
-      <div className="absolute inset-0 z-11 bg-[url('/images/noisy-bg.jpg')] opacity-10 pointer-events-none" />
+    <Card className="group  relative rounded-[40px] overflow-hidden text-white">
+      <div className="absolute inset-0 z-11 bg-[url('/images/background/noise-bg.png')] opacity-60 pointer-events-none" />
 
       <div className="absolute inset-0 z-[10] bg-gradient-to-br from-[#5d5e1e] to-[#3c3d1c]" />
 
@@ -24,21 +24,49 @@ export function ProductCard() {
             <div className="mt-2 h-[1px] bg-white opacity-50 w-full transition-all duration-500 group-hover:w-[212px]" />
           </div>
 
-         <Button
-  className="opacity-0 translate-y-0 transition-all duration-500 group-hover:opacity-100 group-hover:translate-y-8"
->
-  Shop Now
-</Button>
-
+          <Button className="opacity-0 translate-y-0 transition-all duration-500 group-hover:opacity-100 group-hover:translate-y-8">
+            Shop Now
+          </Button>
         </div>
+        {/* Leaf Starts */}
+        <Image
+          src={"/images/carousel/leaf-1.webp"}
+          alt="Leaf top left"
+          width={300}
+          height={300}
+          className="absolute -top-12 left-120 z-30 w-[200px] h-[200px] object-contain 
+             opacity-0 scale-90 
+             transition-all duration-500 ease-in-out 
+             group-hover:opacity-100 group-hover:scale-100"
+        />
+        <Image
+          src={"/images/carousel/leaf-2.webp"}
+          alt="Leaf left bottom"
+          width={300}
+          height={300}
+          className="absolute top-70 left-105 rotate-[240deg] z-50 w-[200px] h-[200px] object-contain 
+             opacity-0 scale-90 
+             transition-all duration-500 ease-in-out 
+             group-hover:opacity-100 group-hover:scale-100"
+        />
+        <Image
+          src={"/images/carousel/leaf-2.webp"}
+          alt="Leaf right bottom"
+          width={300}
+          height={300}
+          className="absolute top-50 left-180 -z-10 w-[200px] h-[200px] object-contain 
+             opacity-0 scale-90 
+             transition-all duration-500 ease-in-out 
+             group-hover:opacity-100 group-hover:scale-100"
+        />
 
-        
+        {/* Leaf Ends*/}
         <Image
           src="/images/product/product_faceoil.png"
           alt="Face Oil"
           width={400}
           height={350}
-          className="object-contain transition-transform duration-500 ease-in-out group-hover:scale-[1.4] group-hover:rotate-[15deg]"
+          className=" object-contain transition-transform duration-500 ease-in-out group-hover:scale-[1.4] group-hover:rotate-[15deg]"
         />
       </CardContent>
     </Card>
